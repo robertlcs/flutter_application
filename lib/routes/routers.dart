2ee3851e7285dd7e23998881c0ce956e9e807179
DuCongcong <17609208486@163.com>
2019-04-24 01:16:22 +0800

    fix:修改音乐页

--- lib/routes/routers.dart
+++ lib/routes/routers.dart
@@ -6,6 +6,7 @@ class Routes {
   static String musicView = "/music-view";
   static String videoView = "/video-view";
   static String newsView = "/news-view";
+  static String accountView = "/account-view";
 
   static void configurationRoutes(Router router) {
     router.define(musicView, handler: homeHandler);
--- lib/views/MusicPage.dart
+++ lib/views/MusicPage.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
-class MusicPage extends StatelessWidget {
+class MusicPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new MusicPage_State();
+  }
+}
+
+class MusicPage_State extends State<MusicPage> {
+
+  static List tableDate = [
+    {'text': '音乐', 'icon': new Icon(Icons.music_note)},
+    {'text': '视频', 'icon': new Icon(Icons.videocam)},
+    {'text': '新闻', 'icon': new Icon(Icons.near_me)},
+    {'text': '我的', 'icon': new Icon(Icons.person)},
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
-          title: new Text('这是测试的首页')
+        title: new Text("音乐馆"),
       ),
       body: new Center(
-        child: new Text('Hello WorDFADld'),
+        child: new Text('这是一个测试文本'),
       ),
     );
   }
